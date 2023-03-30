5871fda97f6aca5b4f0d3174cafec430fe09bfc9
impala <dla4051@naver.com>
2023-03-30 17:52:52 +0900

    프로젝트 생성

---
+++ src/main/java/ving/vingterview/VingterviewApplication.java
@@ -0,0 +1,13 @@
+package ving.vingterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VingterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VingterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ving/vingterview/VingterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ving.vingterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VingterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
