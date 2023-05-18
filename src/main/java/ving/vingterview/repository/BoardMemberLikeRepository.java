4da3ac1e9f141bf9b75023c8ade800420b59c5fe
정찬영 <lms062111@naver.com>
2023-05-18 17:20:05 +0900

    BoardMemberLikeRepository 좋아요 기능 수정

--- src/main/java/ving/vingterview/repository/BoardMemberLikeRepository.java
+++ src/main/java/ving/vingterview/repository/BoardMemberLikeRepository.java
@@ -3,7 +3,10 @@ package ving.vingterview.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import ving.vingterview.domain.LikeType;
+import ving.vingterview.domain.board.Board;
 import ving.vingterview.domain.board.BoardMemberLike;
+import ving.vingterview.domain.comment.Comment;
 
 import java.util.Optional;
 
@@ -13,5 +16,6 @@ public interface BoardMemberLikeRepository extends JpaRepository<BoardMemberLike
     @Query("select bml from BoardMemberLike bml where bml.board.id = :board_id and bml.member.id = :member_id")
     Optional<BoardMemberLike> findByMemberIdAndBoardId( @Param(value = "member_id") Long member_id,@Param(value = "board_id") Long board_id);
 
+    int countByBoardAndLikeStatus(Board board, LikeType status);
 
 }
