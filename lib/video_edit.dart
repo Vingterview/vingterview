3dab121daff8d58fd4143f6ce9be0d897e0d389e
Ssun2zang <mmmmhj@cau.ac.kr>
2023-05-23 00:50:39 +0900

    비디오 수정 페이지 적용

--- lib/video_edit.dart
+++ lib/video_edit.dart
@@ -7,6 +7,7 @@ import 'package:capston/models/globals.dart';
 import 'package:capston/models/questions.dart';
 import 'pick_question.dart';
 import 'record_video.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditVideoPage extends StatefulWidget {
   final Videos video; // 수정할 게시글의 객체
@@ -22,7 +23,7 @@ class _EditVideoPageState extends State<EditVideoPage> {
   String _content;
   String _video_url;
   UploadVideoApi uploadVideoApi = UploadVideoApi();
-  File _video;
+  XFile _video;
   String uri = myUri;
   Questions selectedQuestion;
   String buttonText = "질문을 선택하세요";
@@ -64,97 +65,99 @@ class _EditVideoPageState extends State<EditVideoPage> {
       appBar: AppBar(
         title: Text('Edit Video'),
       ),
-      // body: Padding(
-      //   padding: EdgeInsets.all(16),
-      //   child: Form(
-      //     key: _formKey,
-      //     child: Column(
-      //       crossAxisAlignment: CrossAxisAlignment.stretch,
-      //       children: [
-      //         GestureDetector(
-      //           onTap: () async {
-      //             pickQuestion(context);
-      //           },
-      //           child: Container(
-      //             padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-      //             margin: EdgeInsets.symmetric(vertical: 3),
-      //             decoration: BoxDecoration(
-      //               color: Colors.white,
-      //               borderRadius: BorderRadius.circular(10),
-      //               boxShadow: [
-      //                 BoxShadow(
-      //                   color: Colors.grey.withOpacity(0.5),
-      //                   spreadRadius: 2,
-      //                   blurRadius: 5,
-      //                   offset: Offset(0, 3),
-      //                 ),
-      //               ],
-      //             ),
-      //             child: Text(
-      //               buttonText,
-      //               style: TextStyle(
-      //                 fontSize: 16,
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //         TextFormField(
-      //           initialValue: _content ?? '', // 수정할 게시글의 내용을 초기값으로 설정
-      //           decoration: InputDecoration(labelText: 'Content'),
-      //           validator: (value) {
-      //             if (value.isEmpty) {
-      //               return 'Please enter some content';
-      //             }
-      //             return null;
-      //           },
-      //           onSaved: (value) {
-      //             _content = value;
-      //           },
-      //         ),
-      //         IconButton(
-      //           icon: Icon(Icons.image),
-      //           onPressed: () async {
-      //             _video_url = await uploadVideoApi.pickVideo();
-      //             print(_video_url);
-      //           },
-      //         ),
-      //         IconButton(
-      //           icon: Icon(Icons.camera),
-      //           onPressed: () async {
-      //             _video_url = await Navigator.push(
-      //                 context,
-      //                 MaterialPageRoute(
-      //                     builder: (_) => RecordVideoPage(
-      //                         buttonText: buttonText.toString())));
-      //           },
-      //         ),
-      //         SizedBox(height: 16),
-      //         ElevatedButton(
-      //           child: Text('Update Video'), // 수정 버튼 텍스트 변경
-      //           onPressed: () async {
-      //             if (_formKey.currentState.validate()) {
-      //               _formKey.currentState.save();
-      //               try {
-      //                 // 게시글 업데이트 API 호출
-      //                 await _videoApi.putRequest(
-      //                     widget.video.boardId, // 수정할 게시글의 ID
-      //                     _questionId,
-      //                     _content,
-      //                     _video_url);
-      //                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //                     content: Text('Video updated successfully')));
-      //                 Navigator.pop(context);
-      //               } catch (e) {
-      //                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      //                     content: Text('Failed to update video: $e')));
-      //               }
-      //             }
-      //           },
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  pickQuestion(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 3),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    buttonText,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              TextFormField(
+                initialValue: _content ?? '', // 수정할 게시글의 내용을 초기값으로 설정
+                decoration: InputDecoration(labelText: 'Content'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some content';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _content = value;
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.image),
+                onPressed: () async {
+                  _video = await uploadVideoApi.returnVideo();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.camera),
+                onPressed: () async {
+                  _video = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => RecordVideoPage(
+                              buttonText: buttonText.toString())));
+                  print(_video);
+                  print("dd");
+                },
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                child: Text('Update Video'), // 수정 버튼 텍스트 변경
+                onPressed: () async {
+                  if (_formKey.currentState.validate() && _video != null) {
+                    _formKey.currentState.save();
+                    try {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('영상을 업로드하는데에 1분~10분 이내에 시간이 소요됩니다.')));
+                      Navigator.pop(context);
+                      _video_url = await uploadVideoApi.pickVideo(_video);
+                      print(_video_url);
+                      int boardId = await _videoApi.postVideo(
+                          _questionId, _content, _video_url);
+                      print(boardId);
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //     SnackBar(content: Text('영상이 업로드 되었습니다.')));
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('Failed to pick or post video: $e')));
+                    }
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
